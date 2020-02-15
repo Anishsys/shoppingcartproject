f7af31dc883bc0f3148b38686e177411697638a4
Anishsys <anishnagar61@gmail.com>
2020-02-15 15:47:29 +0530

    Updates

---
+++ precache-manifest.0949ce32a2f43342e696f2dfc9b93334.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ef5ede92cfa547da787f538c8b3ff8",
+    "url": "/Anishsys/shoppingcartproject.git/index.html"
+  },
+  {
+    "revision": "0eb481edf2ba982f9f91",
+    "url": "/Anishsys/shoppingcartproject.git/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "a09ab309bdf95c987249",
+    "url": "/Anishsys/shoppingcartproject.git/static/css/main.4db48624.chunk.css"
+  },
+  {
+    "revision": "0eb481edf2ba982f9f91",
+    "url": "/Anishsys/shoppingcartproject.git/static/js/2.c56f2954.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Anishsys/shoppingcartproject.git/static/js/2.c56f2954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09ab309bdf95c987249",
+    "url": "/Anishsys/shoppingcartproject.git/static/js/main.22d380f3.chunk.js"
+  },
+  {
+    "revision": "6c66d53325819cb17fed",
+    "url": "/Anishsys/shoppingcartproject.git/static/js/runtime-main.749e5705.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Anishsys/shoppingcartproject.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Anishsys/shoppingcartproject.git/precache-manifest.0949ce32a2f43342e696f2dfc9b93334.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Anishsys/shoppingcartproject.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
